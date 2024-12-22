40c7ce8ab0a88abfceb6d6db005d1ef413b73685
Aferist163 <aferist.game.163@gmail.com>
2024-12-22 02:25:39 +0100

    Add  Ui Component🟢

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -33,6 +34,21 @@ namespace Custom_Aura
             this.Close();
         }
 
+
+        private void AllLightsButton_Click(object sender, RoutedEventArgs e)
+        {
+          
+        }
+
+        private void StarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NoneButton_Click(object sender, RoutedEventArgs e)
+        {
+          
+        }
 
         private IAuraSdk sdk;
         public MainWindow()
