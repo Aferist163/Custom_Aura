8bd229b454ea570d05b9242682a73a23c3d6fc26
Aferist163 <aferist.game.163@gmail.com>
2024-12-24 13:25:04 +0100

    Add Ui Rain Effect 🟢

--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -7,8 +7,11 @@ namespace Custom_Aura.MVVM.ViewModel
     {
         public RelayComand All_ViewComand { get; set; }
         public RelayComand Star_ViewComand { get; set; }
+        public RelayComand Rain_ViewComand { get; set; }
+
 
         public All_Color_ViewModel All_C { get; set; }
+        public Rain_Effect_ViewModel Rain_E { get; set; }
 
         public Star_Effect_ViewModel Star_E { get; set; }
 
@@ -28,6 +31,7 @@ namespace Custom_Aura.MVVM.ViewModel
         {
             All_C = new All_Color_ViewModel();
             Star_E = new Star_Effect_ViewModel();
+            Rain_E = new Rain_Effect_ViewModel();
             CurrentView = All_C;
 
             Star_ViewComand = new RelayComand(o => 
@@ -38,6 +42,11 @@ namespace Custom_Aura.MVVM.ViewModel
             All_ViewComand = new RelayComand(o =>
             {
                 CurrentView = All_C;
+            });
+
+            Rain_ViewComand = new RelayComand(o =>
+            {
+                CurrentView = Rain_E;
             });
         }
     }
